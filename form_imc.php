04b67362f07de0c4311af5bca6fd820799b1ecdf
Julio <julianoandradeo10@gmail.com>
2023-08-27 20:28:31 -0300

    Commit dos arquivos

--- form_imc.php
+++ form_imc.php
@@ -1,1 +1,44 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculadora de IMC</title>
+</head>
+<body>
+    <h1>Calculadora de IMC</h1>
+    <form method="post" action="">
+        Peso (kg): <input type="text" name="peso"><br>
+        Altura (m): <input type="text" name="altura"><br>
+        <input type="submit" value="Calcular">
+    </form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $peso = str_replace(",", ".", $_POST["peso"]);
+        $altura = str_replace(",", ".", $_POST["altura"]);
+        
+        $imc = $peso / ($altura * $altura);
+        $imc = round($imc, 2);
+        
+        echo "<h2>Resultado</h2>";
+        echo "Seu IMC é: $imc<br>";
+        
+        if ($imc < 17) {
+            echo "Situação: Muito abaixo do peso";
+        } elseif ($imc >= 17 && $imc < 18.5) {
+            echo "Situação: Abaixo do peso";
+        } elseif ($imc >= 18.5 && $imc < 25) {
+            echo "Situação: Peso normal";
+        } elseif ($imc >= 25 && $imc < 30) {
+            echo "Situação: Acima do peso";
+        } elseif ($imc >= 30 && $imc < 35) {
+            echo "Situação: Obesidade I";
+        } elseif ($imc >= 35 && $imc < 40) {
+            echo "Situação: Obesidade II (severa)";
+        } else {
+            echo "Situação: Obesidade III (mórbida)";
+        }
+    }
+    echo "<br><br>";
+    echo "<a href=\"index.html\">Voltar ao Index</a>";
+    ?>
+</body>
+</html>
--- form_notas.php
+++ form_notas.php
@@ -1,1 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculadora de Média</title>
+</head>
+<body>
+    <h1>Calculadora de Média</h1>
+    <form method="post" action="">
+        Nome do Aluno: <input type="text" name="nome"><br>
+        Nota 1: <input type="text" name="nota1"><br>
+        Nota 2: <input type="text" name="nota2"><br>
+        <input type="submit" value="Calcular">
+    </form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nome = $_POST["nome"];
+        $nota1 = $_POST["nota1"];
+        $nota2 = $_POST["nota2"];
+        
+        $media = ($nota1 + $nota2) / 2;
+        $media = round($media, 2);
+        
+        echo "<h2>Resultado</h2>";
+        echo "Aluno: $nome<br>";
+        echo "Média: $media<br>";
+        
+        if ($media < 4) {
+            echo "Situação: Reprovado";
+        } elseif ($media >= 4 && $media < 6) {
+            echo "Situação: Exame final";
+        } else {
+            echo "Situação: Aprovado";
+        }
+    }
+    echo "<br><br>";
+    echo "<a href=\"index.html\">Voltar ao Index</a>";
+    ?>
+</body>
+</html>
 
